55dd16d3bf35b2b639fd2a1551a439d975a392ba
Oleg Yakut <ollorukolo@rambler.ru>
2023-01-31 15:13:49 +0200

    2023_01_31

---
+++ 11.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+char pol[][7]={"what","are","youare"};
+for (int i=0; i<3; i++)
+{
+	for (int j=0; j<7; j++)	
+	{
+		cout<<pol[i][j];
+	}
+	cout<<"akljg;lkajglksadjl;";
+}
+return 0;
+}
